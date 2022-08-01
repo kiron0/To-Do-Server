7774bdfbc1ff7f487cd34d386178831e7d7a41bc
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-08-02 01:22:42 +0600

    home page designed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -261,7 +262,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("K Task TO DO is Calling You!");
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.listen(port, () => {
